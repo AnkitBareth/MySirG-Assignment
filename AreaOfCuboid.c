d34bb80f434ac96531cf5311abeb4ec2a8f31075
AnkitBareth <108222005+AnkitBareth@users.noreply.github.com>
2025-02-19 12:03:33 +0530

    Delete AreaOfCuboid.c

--- AreaOfCuboid.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int l,b,h;
-    int area;
-    printf("Enetr The leanth,Breadth,Hight Of Cuboid : \n");
-    scanf("%d%d%d",&l,&b,&h);
-    printf("Area Of Cuboid Is %d",area=l*b*h);
-    return 0;
-}
